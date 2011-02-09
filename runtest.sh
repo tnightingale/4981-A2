a013141b1f6cccef83c898dc566fb70bcd54f97f
Tom Nightingale <tom@tnightingale.com>
2011-02-08 23:42:04 -0800

    updated testing to work with mac os

--- runtest.sh
+++ runtest.sh
@@ -1,22 +1,20 @@
-#! /bin/bash
+#!/bin/bash
 
 os=`uname`
 command="./client tmp.txt"
+i=$1
 
-for ((i=$1; i > 0; i = i - 2)); do
-    echo "Starting client, priority: $i"
-    if [[ $os == 'Linux' ]]; then
-      gnome-terminal -e "$command $i"
-    elif [[ $os == 'Darwin' ]]; then
-      appletermexec "$command $1"
-    fi
-    sleep $2
-  done
+while [ $i -gt 0 ]; do
+  echo "Starting client, priority: $i"
 
-function appletermexec {   
-  osascript <<END 
-  tell application "Terminal"
-    do script "cd \"`pwd`\";$1;exit"
-  end tell
-  END
-}
+  if [[ $os == 'Linux' ]]; then
+    gnome-terminal -e "$command $i"
+  elif [[ $os == 'Darwin' ]]; then
+    echo "Mac OS"
+    ./term -x "$command $i"
+  fi
+
+  sleep $2
+  let i-=2
+done
+
